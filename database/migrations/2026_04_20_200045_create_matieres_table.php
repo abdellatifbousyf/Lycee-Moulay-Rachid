c917e2fadc9a70d069d27a1950a9d58776c9ae53
Abdellatif <abdellatifbousyf06@gmail.com>
2026-04-20 21:03:05 +0100

    data base

---
+++ database/migrations/2026_04_20_200045_create_matieres_table.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('matieres', function (Blueprint $table) {
+            $table->id();
+            $table->string('nom_mat', 25)->unique(); // ✅ مستحسن منع تكرار سمّية المادة
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('matieres');
+    }
+};
